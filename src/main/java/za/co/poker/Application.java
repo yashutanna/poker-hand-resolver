bdd6a53dd8d659c8de2793852ca1cd2f1997073e
yashu <yashu.tanna@outlook.com>
2018-11-16 12:56:04 +0200

    updated main file to have array of hands declared

--- src/main/java/za/co/poker/Application.java
+++ src/main/java/za/co/poker/Application.java
@@ -3,10 +3,27 @@ package za.co.poker;
 import za.co.poker.dealer.Judge;
 import za.co.poker.hands.PokerHand;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        List<String> hands = Arrays.asList(
+            "3C,3H,3S,2C,2S",
+            "3C,3H,3S,3D,2S"
+        );
+
         Judge judge = new Judge();
-        PokerHand hand = judge.deal("3C,3H,3S,2C,2S");
-        System.out.println(judge.assessHand(hand).print());
+        hands.forEach(hand -> {
+            System.out.println("assessing hand: " + hand);
+            PokerHand pokerHand = null;
+            try {
+                pokerHand = judge.deal(hand);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(judge.assessHand(pokerHand).print());
+            System.out.println();
+        });
     }
 }
